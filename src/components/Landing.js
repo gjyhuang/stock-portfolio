a341d1b1cdac8db87ea7f2f515cf70c63ba5a451
gjyhuang <grace.jy.huang@gmail.com>
2020-02-14 14:39:54 -0500

    [css] landing page styling + adjustments

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -5,11 +5,14 @@ import {Login, Signup} from './AuthForm'
 
 function Landing() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome!
-        </p>
+    <div className="landing flex-centered-all">
+      <header className="page-header flex-centered-all flex-dir-col">
+        <div className="font-header">Welcome!</div>
+        <div className="divider-20px" />
+        <div className="landing-login flex-centered-all flex-dir-row flex-wrap flex-space-btw">
+          <div className="font-subheader">Sign Up</div>
+          <div className="font-subheader">Log in</div>
+        </div>
         <Login />
         <Signup />
       </header>
