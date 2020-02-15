61d8c1e57bd7525d8d84dd99640b1c1654d3a3d7
gjyhuang <grace.jy.huang@gmail.com>
2020-02-15 13:20:41 -0500

    [add] page loader component + css

---
+++ src/components/screens/PageLoader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PageLoader = (props) => {
+  return (
+    <div className="preloader">
+      <div className="loader">
+        <span></span>
+      </div>
+    </div>
+  )
+}
+
+export default PageLoader;
