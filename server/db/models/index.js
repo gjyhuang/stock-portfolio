23dcd4bd051c4211fa7829cc23494eab76ec5cee
gjyhuang <grace.jy.huang@gmail.com>
2020-02-16 22:23:59 -0500

    [add] api routes for transactions

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,7 +4,7 @@ const Stock = require('./stock');
 const TransactionHistory = require('./transactionHistory');
 const Transaction = require('./transaction')
 
-// each user has one portfolio
+// each user has one portfolio and transactions history
 // put the foreign key (portfolio, history) on the user table
 User.Portfolio = User.belongsTo(Portfolio);
 User.belongsTo(Portfolio);
@@ -17,7 +17,6 @@ Portfolio.hasMany(Stock);
 
 // each transaction is unique and belongs to one portfolio
 // put the foreign key (single transaction) on the history table
-// put the foreign key (history) on the user table
 TransactionHistory.hasMany(Transaction);
 
 module.exports = {
