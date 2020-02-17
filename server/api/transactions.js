e3efbbe0da029d9e75818f92eeebd8b597a7c056
gjyhuang <grace.jy.huang@gmail.com>
2020-02-16 21:38:18 -0500

    [add] api routes for transactions

--- server/api/transactions.js
+++ server/api/transactions.js
@@ -5,8 +5,34 @@ module.exports = router;
 
 router.get('/', async (req, res, next) => {
   try {
-    const transactions = await TransactionHistory.findAll();
+    const transactions = await TransactionHistory.findAll({
+      include: [{ model: Transaction }]
+    });
     res.json(transactions);
+  } catch (err) {
+    next(err);
+  }
+})
+
+// add selected stock upon purchase to user transaction history
+router.post('/:id', async (req, res, next) => {
+  try {
+    // make sure the user is logged in via passport's req.user
+    if (!req.user) res.sendStatus(401);
+    const { symbol, name, priceAtPurchase, quantity, date } = req.body;
+    const userTransactions = await TransactionHistory.findOne({
+      where: { id: req.params.id },
+      include: [{ model: Transaction }]
+    })
+    const newTransaction = await Transaction.create({
+      symbol,
+      name,
+      priceAtPurchase,
+      quantity,
+      date,
+    });
+    await userTransactions.addTransaction(newTransaction);
+    res.json(userTransactions);
   } catch (err) {
     next(err);
   }
@@ -15,8 +41,11 @@ router.get('/', async (req, res, next) => {
 // get user's transactions
 router.get('/:id', async (req, res, next) => {
   try {
-    const userTransactions = await TransactionHistory.findAll({
-      where: { id: req.user.id },
+    // make sure the user is logged in via passport's req.user
+    if (!req.user) res.sendStatus(401);
+    const userTransactions = await TransactionHistory.findOne({
+      where: { id: req.params.id },
+      include: [{ model: Transaction }]
     });
     res.json(userTransactions);
   } catch (err) {
