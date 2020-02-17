b0c4714ce70c5318c40a1b49e5fd95c4355613d8
gjyhuang <grace.jy.huang@gmail.com>
2020-02-16 23:42:41 -0500

    [add] portfolio to redux store, thunk + reducer for fetching portfolio; removed stock from store

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,10 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import portfolio from './portfolio'
 import transactions from './transactions'
 
-const reducer = combineReducers({user, transactions})
+const reducer = combineReducers({user, portfolio, transactions})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
@@ -13,4 +14,5 @@ const store = createStore(reducer, middleware)
 
 export default store
 export * from './user'
+export * from './portfolio'
 export * from './transactions'
